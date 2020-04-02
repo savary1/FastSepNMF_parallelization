73a66920bef9fbc768cdd6038257842005159ece
savary1 <david.savarymartinez@gmail.com>
2020-04-02 19:31:35 +0200

    Vectorized version now prints coordinates

--- FastSEPNMF_C_vec/FastSEPNMF.c
+++ FastSEPNMF_C_vec/FastSEPNMF.c
@@ -208,8 +208,7 @@ int main (int argc, char* argv[]){
 
 	printf("Endmembers:\n");
     for(i = 0; i < endmembers; i++){
-		//printf("%ld \t- %ld \t- Coordenadas: (%ld,%ld) \t- Valor: %f\n", i, J[i],(J[i] / cols),(J[i] % cols), normM1[J[i]]);
-        printf("%ld \t- %ld\n", i, J[i]);
+		printf("%ld \t- %ld \t- Coordenadas: (%ld,%ld) \t- Valor: %f\n", i, J[i],(J[i] / cols),(J[i] % cols), normM1[J[i]]);
     }
 
 	printf("Total time:	\t%.5f segundos\n", secsFin);
